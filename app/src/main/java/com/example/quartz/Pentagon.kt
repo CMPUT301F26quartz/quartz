b8dd6b5bb238b65c9d8fbb6f2c1a5532e762916f
cessa <cess@cess-2.local>
2026-09-23 18:43:04 -0600

    p

--- app/src/main/java/com/example/quartz/Pentagon.kt
+++ app/src/main/java/com/example/quartz/Pentagon.kt
@@ -1,5 +1,4 @@
 package com.example.quartz
 
 class Pentagon (x: Int, y: Int): Shape(x,y){
-    val color: String = "red"
 }
--- app/src/main/java/com/example/quartz/Shape.kt
+++ app/src/main/java/com/example/quartz/Shape.kt
@@ -1,3 +1,5 @@
 package com.example.quartz
 
-abstract class Shape(val x: Int, val y: Int)
+abstract class Shape(val x: Int, val y: Int) {
+    val color : String = "pink"
+}
